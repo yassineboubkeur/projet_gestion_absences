a42883b8fd88c357a382206f26b3e125c349e99e
yassine <boubkeuryassine2@gmail.com>
2025-07-26 09:46:01 +0100

    create frontend and backend

---
+++ backend/src/main/java/com/example/projet_gestion_absences/ProjetGestionAbsencesApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_gestion_absences;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetGestionAbsencesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetGestionAbsencesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/projet_gestion_absences/ProjetGestionAbsencesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_gestion_absences;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetGestionAbsencesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
